dad1a6ff46a57537e5f4952346f9aff5ea9519db
Ken Lu <ken@seeice.com>
2013-03-11 23:46:55 -0400

    added user_id to album

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130311020028) do
+ActiveRecord::Schema.define(:version => 20130312034022) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "images", :force => true do |t|
