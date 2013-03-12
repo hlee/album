54138722b41e4e91608a3fd9adb2e4fca5e5aa4c
Ken Lu <ken@seeice.com>
2013-03-11 23:45:39 -0400

    Added nested attribute

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,6 @@
 class Album < ActiveRecord::Base
   attr_accessible :description, :title
-  has_many :images
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images
+  
 end
